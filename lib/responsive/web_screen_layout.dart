98dd5bf4db8408d62dc88f989d92bc488e01d55a
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2024-01-08 14:04:24 +0530

    web appbar ui complete

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_clone/utils/colors.dart';
+import 'package:google_clone/utils/style.dart';
 
 class WebLayout extends StatelessWidget {
   const WebLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('hello from web'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Gmail',
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Images',
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          //const SizedBox(width: 10),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset('assets/images/more-apps.svg',
+                colorFilter:
+                    const ColorFilter.mode(primaryColor, BlendMode.srcIn)),
+          ),
+          const SizedBox(width: 10),
+          MaterialButton(
+            onPressed: () {},
+            color: const Color(0xff1A73EB),
+            child: const Text(
+              'Sign in',
+              style: TextStyle(color: primaryColor),
+            ),
+          ),
+          const SizedBox(width: 10),
+        ],
+      ),
+      body: const Text('hello from web'),
     );
   }
 }
