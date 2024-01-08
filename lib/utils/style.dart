3b9e91ea2c18d096095dd5bea9eba66c91e78e5d
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2024-01-08 12:34:15 +0530

    create theme and make screen layout for web and mobile

---
+++ lib/utils/style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:google_clone/utils/colors.dart';
+
+final kDarkTheme = ThemeData.dark().copyWith(
+  scaffoldBackgroundColor: backgroundColor,
+);
