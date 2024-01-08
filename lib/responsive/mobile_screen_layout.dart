230ee8aac8155375577979ef1ca9a4d903e4b05e
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2024-01-08 19:53:33 +0530

    in mobile layout complete upto mobile footer

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,12 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../utils/colors.dart';
+import '../widgets/mobile/mobile_footer.dart';
+import '../widgets/search.dart';
+import '../widgets/translation_buttons.dart';
+import '../widgets/web/search_buttons.dart';
 
 class MobileLayout extends StatelessWidget {
   const MobileLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('hello from mobile'),
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        title: SizedBox(
+          width: size.width * 0.34,
+          child: const DefaultTabController(
+              length: 2,
+              child: TabBar(
+                labelColor: blueColor,
+                unselectedLabelColor: Colors.grey,
+                indicatorColor: blueColor,
+                tabs: [
+                  Tab(text: 'ALL'),
+                  Tab(text: 'IMAGES'),
+                ],
+              )),
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.grey,
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Gmail',
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Images',
+              style: TextStyle(
+                color: primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          //const SizedBox(width: 10),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset('assets/images/more-apps.svg',
+                colorFilter:
+                    const ColorFilter.mode(primaryColor, BlendMode.srcIn)),
+          ),
+          const SizedBox(width: 10),
+          MaterialButton(
+            onPressed: () {},
+            color: const Color(0xff1A73EB),
+            child: const Text(
+              'Sign in',
+              style: TextStyle(color: primaryColor),
+            ),
+          ),
+          const SizedBox(width: 10),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 5, right: 5),
+        child: Column(
+          children: [
+            SizedBox(height: size.height * 0.25),
+            const Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Search(),
+                      SizedBox(height: 20),
+                      SearchButtons(),
+                      SizedBox(height: 20),
+                      TranslationButtons(),
+                    ],
+                  ),
+                  MobileFooter(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/utils/style.dart
+++ lib/utils/style.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_clone/utils/colors.dart';
 
-final kDarkTheme = ThemeData.dark().copyWith(
+final kDarkTheme = ThemeData.dark(useMaterial3: false).copyWith(
   scaffoldBackgroundColor: backgroundColor,
 );
